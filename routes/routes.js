5d78f14bb0e45b08a017a580740b51b54d5f77a9
Vitor Moisés de Freitas Rey <vitor@atonsystems.com.br>
2022-05-02 12:24:41 -0300

    [Routes]  routes file adjustment

--- routes/routes.js
+++ routes/routes.js
@@ -2,11 +2,5 @@ const connection = require("../database/connection");
 const express = require("express");
 const router = express.Router();
 const UserController = require("../controllers/UserController");
-router
-  // Rotas usuário.
-  .post("/api/createUser", UserController.newUser)
-  .put("/api/updateUser", UserController.UpdateUser)
-  .post("/api/login", UserController.login)
-  .get("/api/getInfos", UserController.getInfos);
 
 module.exports = router;
