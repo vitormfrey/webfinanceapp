d48374bbd9d236797b1f40b8973fdfaa6f8aea3a
vitormfrey <vitormoises.rey@gmail.com>
2022-05-10 22:45:44 -0300

    [Helper] Created helper "containsBalance"
    This helper verify if balance value is valid

--- Helper/helpers.js
+++ Helper/helpers.js
@@ -11,4 +11,18 @@ function isValid(values) {
   });
 }
 
+function containsBalance(balance) {
+  let result = { success: true, value: 0, message: "" };
+  if (balance.length > 0) result.value = parseFloat(balance[0].value);
+
+  if (result.value > 0) {
+    result.message =
+      "This account contains balance, please transfer this balance to another account or zero your balance";
+  } else {
+    result.success = false;
+  }
+
+  return result;
+}
 module.exports.isValid = isValid;
+module.exports.containsBalance = containsBalance;
