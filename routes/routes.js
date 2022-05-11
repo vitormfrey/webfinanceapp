4c2922c28a86d10e84c9f9831589ebed2049af37
vitormfrey <vitormoises.rey@gmail.com>
2022-05-10 22:48:05 -0300

    [Routes] added routes - Account

--- routes/routes.js
+++ routes/routes.js
@@ -2,9 +2,15 @@ const express = require("express");
 const router = express.Router();
 var knex = require("../database/knex");
 const UserController = require("../controllers/UserController");
+const AccountController = require("../controllers/AccountController");
 
 //User routes
-router.post("/api/createUser", UserController.createUser);
-router.put("/api/updateUser", UserController.updateUser);
+router.post("/api/createUser", UserController.CreateUser);
+router.put("/api/updateUser", UserController.UpdateUser);
+router.delete("/api/deleteUser", UserController.DeleteUser);
+//Account routes
+router.post("/api/createAccount", AccountController.CreateAccount);
+router.get("/api/getAccount", AccountController.GetAccount);
+router.put("/api/inactivateAccount", AccountController.InactivateAccount);
 
 module.exports = router;
